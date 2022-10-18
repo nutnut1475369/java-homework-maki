3cb0230b44b30ad6b98f67df7bff3e139e3117d2
Nutapon <nutnut1475369@hotmail.com>
2022-10-18 13:12:37 +0700

    Implement digital root

--- src/main/java/com/wongnai/interview/wamup/DigitalRoot.java
+++ src/main/java/com/wongnai/interview/wamup/DigitalRoot.java
@@ -1,5 +1,7 @@
 package com.wongnai.interview.wamup;
 
+import java.util.InputMismatchException;
+
 public class DigitalRoot {
 	public Object check(long number) {
 		//TODO: Warmup practice => Implement this method to find out the digital root number of the input.
@@ -14,7 +16,17 @@ public class DigitalRoot {
 		// -------------------------------
 		// -------------------------------
 		// All test case in DigitalRootTest must be passed.
-
-		return null;
+		if(number < 0) throw new InputMismatchException("Invalid input");
+		long sum = 0;
+		while(number > 0){
+			long digit = number%10;
+			number = number/10;
+			sum += digit;
+		}
+		if(sum > 9) {
+			return check(sum);
+		}else{
+			return sum;
+		}
 	}
 }
