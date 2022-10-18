b36804119a85c575754d6d8a1aeb8b46124d159c
Nutapon <nutnut1475369@hotmail.com>
2022-10-18 16:37:40 +0700

    Implement database relate method

--- src/main/java/com/wongnai/interview/movie/MovieRepository.java
+++ src/main/java/com/wongnai/interview/movie/MovieRepository.java
@@ -22,6 +22,5 @@ public interface MovieRepository extends CrudRepository<Movie, Long> {
 	 * 		a user query keyword
 	 * @return list of movie
 	 */
-	@Query("SELECT m FROM Movie m where m.name LIKE %:keyword%")
-	List<Movie> findByNameContains(@Param("keyword") String keyword);
+	List<Movie> findByNameContainsIgnoreCase(@Param("keyword") String keyword);
 }
